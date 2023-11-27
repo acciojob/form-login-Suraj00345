20a15223fb7eb2a35ba8b21e7fc138a6f090fefd
Suraj Kumar Manna <126694317+Suraj00345@users.noreply.github.com>
2023-11-27 18:19:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function getFormvalue() {
-    //Write your code here
 
+   function getFormvalue() {
+var x=document.getElementById("form1");
+let name = ""
+for (var i=0;i<x.length;i++) {
+if (x.elements[i].value!='Submit')
+name += x.elements[i].value + " "
 }
+
+alert(name.substring(0,name.length-1));
+}
+
+
